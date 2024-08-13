ea7d69f5c3c33899283c41d850fc833a5bd0dee6
Lívia Lopes <156115775+pequitotito@users.noreply.github.com>
2024-08-13 08:10:54 -0300

    Add files via upload
    Versão inicial e testes do jogo. Ainda não funcional e sendo modificado, cheio de erros.

---
+++ testes.py
@@ -0,0 +1,13 @@
+
+
+class Jogador:
+    def __init__(self, usuario, player="computador",  vida=1, funcao=""):
+        self.funcao = funcao
+        self.vida = vida
+        self.player = player
+        self.usuario = usuario
+
+    def escolher_funcao(player, funcao):
+        func = ""
+        if 
+
