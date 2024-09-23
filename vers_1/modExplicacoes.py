b177c8c549c5697b167e7fdd85d65866f1f42226
Lívia Lopes <156115775+pequitotito@users.noreply.github.com>
2024-09-22 22:01:21 -0300

    Update modExplicacoes.py

--- vers_1/modExplicacoes.py
+++ vers_1/modExplicacoes.py
@@ -20,33 +20,31 @@ MMMM                     MMMM     MMMM                      MMMM    MMMM        
     print("\nBem-vindo ao jogo Máfia! 🕵  🔪 🩸") 
     sleep(0.25)
     explicacao = input("Gostaria de uma explicação antes de iniciar o jogo?\nResponda com 'Sim' ou 'Não': ").strip().lower()
-    if explicacao == 'sim' or explicacao == 's':
-        return explicacao()
+    if explicacao in ['s', 'sim']:
+        explicacao()
     else:
         sleep(0.25)    
         print("Já que você não precisa de mais explicações... Vamos ao jogo!")
 
 def explicacao() -> None:
-    sleep(0.25), print("\nHá nesta cidade um terrível assassino... Ele busca assassinar todos os jogadores a sangue frio\n",
-                       "e causar discórdia entre os moradores da cidade.")
-    
-    sleep(0.5), print("Há um narrador que dará informações durante todo o jogo. Logo no início são distribuídos papéis entre\n",
-                      "os jogadores. Eles podem ser:\n- Mafioso/Assassino\n- Médico\n- Xerife\n- Cidadão")
-    
-    sleep(0.5), print("Após a distribuição dos papéis o jogo começa...\n\n")
+    mensagens = [
+        "\nHá nesta cidade um terrível assassino... Ele busca assassinar todos os jogadores a sangue frio\ne causar discórdia entre os moradores da cidade\n",
+        "Há um narrador que dará informações durante todo o jogo. Logo no início sçao distribuídos papéis entre os jogadores.\nOs papéis são:\nMafioso\nMédico\nXerife\nCidadão\n\n",
+        "Após a distribuição dos papéis o jogo começa...\n\n",
+        "Existem três papéis que podem agir durante a noite: o assassino, o médico e o xerife.\nO assassino escolherá alguém para eliminar durante a noite. Após ele, o médico age para proteger alguém durante a noite.\n",
+        "Caso o protegido do médico coincida com a pessoa que o mafioso tentou assassinar durante a noite, esta pessoa continuará sã e salva.\n",
+        "O xerife age depois investigando quem ele acha que é o assassino.\nO narrador dirá se ele está certo ou não...\n",
+        "O narrador ditará quando a cidade amanhecer. Quando isso ocorrer, todas os cidadãos se reunirão para tentar achar o assassino.\nPara isso, ocorrerá uma discussão e posteriormente uma votação para eliminar alguém. \n",
+        "O acusado pode tentar se defender e após a sua defesa, os jogadores decidem se ele é culpado ou não.\n",
+        "Caso decidam eliminá-lo e ele for o assassino, o jogo termina com vitória da Cidade!\n",
+        "Caso decidam eliminá-lo e ele não for o assassino... Mais uma noite chega e o assassino irá agir.\n",
+        "Se não decidirem eliminar o jogador, mais uma discussão ocorre até que alguém seja escolhido para morrer.\n",
+        "O jogo termina com a vitória da Cidade ou do Mafioso! Boa sorte e bom jogo!!\n"
+    ]
+    for msg in mensagens:
+        sleep(1.5)
+        print(msg)
 
-    sleep(0.5), print("Existem três papéis que podem agir durante a noite: o assassino, o médico e o xerife.\nO assassino escolherá",
-                     "alguém para eliminar durante a noite. Após ele, o médico age para proteger alguém durante a noite.")
-    
-    sleep(0.5), print("Caso o protegido do médico coincida com a pessoa que o mafioso tentou assassinar durante a noite, esta pessoa continuará sã e salva.\n")
-    sleep(0.5), print("O xerife age depois investigando quem ele acha que é o assassino.\nO narrador dirá se ele está certo ou não...")
-    sleep(0.5), print("O narrador ditará quando a cidade amanhecer. Quando isso ocorrer, todas os cidadãos se reunirão para tentar achar o assassino.",
-                      "\nPara isso, ocorrerá uma discussão e posteriormente uma votação para eliminar alguém. \n")
-    sleep(0.5), print("O acusado pode tentar se defender e após a sua defesa, os jogadores decidem se ele é culpado ou não.")
-    sleep(0.5), print("Caso decidam eliminá-lo e ele for o assassino, o jogo termina com vitória da Cidade!")
-    sleep(0.5), print("Caso decidam eliminá-lo e ele não for o assassino... Mais uma noite chega e o assassino irá agir.")
-    sleep(0.5), print("Se não decidirem eliminar o jogador, mais uma discussão ocorre até que alguém seja escolhido para morrer.\n")
-    sleep(0.5), print("O jogo termina com a vitória da Cidade ou do Mafioso! Boa sorte e bom jogo!!\n")
     
 def mensagem_fim_de_jogo(vitoria: bool):
     '''Caso seja vitória da Cidade, exibe que foi vitória dos jogadores. Caso seja vitória do mafioso, foi derrota dos jogadores.'''
